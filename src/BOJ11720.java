31ccd06d6f3d3800fe9b7008bda941961b0bd4e9
justinnn980 <justinnn980@gmail.com>
2024-12-21 00:54:58 +0900

    BOJ11720 풀기 완료

---
+++ src/BOJ11720.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BOJ11720 {
+
+    public static void main(String[] args) {
+        long m = 0;
+        Scanner sc = new Scanner(System.in); /// 값넣기 준비
+        int i = sc.nextInt();  /// i 값넣기
+        String s = sc.next(); /// s 값넣기
+        char[] str = s.toCharArray(); /// 하나하나 나누기
+
+        for(int j = 0; j < i; j++) {
+            ///아스키코드 삭제
+            m = m + str[j] - 48;
+        }
+        System.out.println(m);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+//    public static void main(String[] args) {
+//        ///초기화 하기
+//        ArrayList<Edge> list[] = new ArrayList[10];
+//
+//        for (int i = 0; i < 10; i++) {
+//            list[i] = new ArrayList<Edge>();
+//        }
+//
+//        /// 그래프 데이터 저장하기 e = 저장할 에지
+//        for (int i = 0; i < E; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            int s = Integer.parseInt(st.nextToken());
+//            int e = Integer.parseInt(st.nextToken());
+//            int v = Integer.parseInt(st.nextToken());
+//            list[s].add(new Edge(e, v));
+//        }
+//
+//        for (int i = 0; i < list[2].size(); i++) {
+//            Edge tmp = list[2].get(i);
+//            int next = tmp.endNode;
+//            int value = tmp.value;
+//
+//        }
+//    }
+}
