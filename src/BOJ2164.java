a1ce4c9a834604ea372bf5ff0703980172f7664d
justinnn980 <justinnn980@gmail.com>
2025-01-02 23:51:35 +0900

    BOJ2164 카드2(큐를 활용한 문제) JAVA

--- src/BOJ2164.java
+++ src/BOJ2164.java
@@ -1,5 +1,22 @@
-public class BOJ2164 {
-    public static void main(String[] args) {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
+public class BOJ2164 { // 큐 자료구조
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> q = new LinkedList<>();
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            q.add(i);
+        }
+        while(q.size() > 1) {
+            q.poll();
+            int temp = q.poll();
+            q.add(temp);
+            ///q.add(q.poll());
+
+        }
+        System.out.println(q.poll());
     }
 }
