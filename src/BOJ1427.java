6e02fd580d5ba1cd9d0c94c91588244d4cf02f09
justinnn980 <justinnn980@gmail.com>
2025-01-06 23:56:45 +0900

    BOJ1427 소트인사이드 (내림차순 버블 정렬 구현해보기) JAVA

---
+++ src/BOJ1427.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BOJ1427 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String o = sc.next(); /// o 값넣기
+        char[] str = o.toCharArray(); /// 하나하나 나누기
+
+        /// 직접 버블 정렬 방식으로 내림차순 구현
+        for (int i = 0; i < str.length - 1; i++) {
+            for (int j = i + 1; j < str.length; j++) {
+                if (str[i] < str[j]) {
+                    char temp = str[i];
+                    str[i] = str[j];
+                    str[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < str.length; i++) {
+            System.out.print(str[i]);
+        }
+    }
+}
