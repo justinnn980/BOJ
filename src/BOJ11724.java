fa82b90264bff47b36ed72d4dab9d164426db1c4
justinnn980 <justinnn980@gmail.com>
2025-01-07 23:00:52 +0900

    BOJ11724 연결 요소의 개수 (DFS) JAVA

---
+++ src/BOJ11724.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BOJ11724 {
+    static boolean visited[];
+    static ArrayList<Integer>[] A;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        visited = new boolean[n+1];
+        A = new ArrayList[n+1];
+
+        for (int i = 1; i < n+1; i++) {
+            A[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            A[u].add(v);
+            A[v].add(u);
+        }
+        int count = 0;
+        for (int i = 1; i < n+1; i++) {
+            if (!visited[i]) {
+                count++;
+                DFS(i);
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static void DFS(int e) {
+        if (visited[e]) {
+            return;
+        } visited[e] = true;
+        for (int j : A[e]) {///현재 노드와 연결되어 있는 노드
+            if (!visited[j]) {///아직 탐색하지 않은 노드가 있다면 자신을 다시 실행
+                DFS(j);
+            }
+        }
+    }
+}
