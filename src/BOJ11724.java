645618974ec8ddeda33b05b11cbd15a2541c8d42
justinnn980 <justinnn980@gmail.com>
2025-01-09 22:39:15 +0900

    BOJ11724 연결 요소의 개수 (DFS) JAVA

--- src/BOJ11724.java
+++ src/BOJ11724.java
@@ -32,11 +32,12 @@ public class BOJ11724 {
     }
 
     private static void DFS(int e) {
-        if (visited[e]) {
+        if (visited[e]) { ///방문노드이면
             return;
-        } visited[e] = true;
-        for (int j : A[e]) {///현재 노드와 연결되어 있는 노드
-            if (!visited[j]) {///아직 탐색하지 않은 노드가 있다면 자신을 다시 실행
+        }
+        visited[e] = true; ///방문 할 노드
+        for (int j : A[e]) { ///현재 노드와 연결되어 있는 노드 탐색
+            if (!visited[j]) { ///아직 탐색하지 않은 노드가 있다면 자신을 다시 실행
                 DFS(j);
             }
         }
