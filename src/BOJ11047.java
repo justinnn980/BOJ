3a352d02f6b0367d507bb253bdb70c27f94ec620
justinnn980 <justinnn980@gmail.com>
2025-01-12 23:07:41 +0900

    BOJ11047 동전 0 (그리디) JAVA 완료

---
+++ src/BOJ11047.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BOJ11047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= k) {
+                int re = k / arr[i];
+                result = result + re;
+                k = k % arr[i];
+            }
+        }
+        System.out.println(result);
+    }
+}
