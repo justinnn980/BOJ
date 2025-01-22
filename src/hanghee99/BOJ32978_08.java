cca2363901e602d6c9931fff98582a68188084b8
justinnn980 <justinnn980@gmail.com>
2025-01-22 14:48:51 +0900

    항해99 8일차 32978 아 맞다 마늘 JAVA

---
+++ src/hanghee99/BOJ32978_08.java
@@ -0,0 +1,29 @@
+package hanghee99;
+
+import java.util.Scanner;
+
+public class BOJ32978_08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+        for (int i = 0; i < n-1; i++) {
+            String s = sc.next();
+            for (int j = 0; j < n; j++) {
+                if (s.equals(arr[j])) {
+                    arr[j] = null;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != null) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
+}
