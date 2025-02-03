f4040cda471e5c478f64c2c150fefd5c62ad3b2f
justinnn980 <justinnn980@gmail.com>
2025-02-04 01:18:48 +0900

    항해99 11일차 BOJ10828 스택 JAVA

--- src/hanghee99/BOJ10828_11.java
+++ src/hanghee99/BOJ10828_11.java
@@ -3,17 +3,72 @@ package hanghee99;
 import java.util.Scanner;
 
 public class BOJ10828_11 {
+    public static int[] stack;
+    public static int size = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+
         int N = sc.nextInt();
 
-        for (int i = 1; i <= N; i++) {
+        stack = new int[N];
 
-            if ()
+        for (int i = 0; i < N; i++) {
+            String s = sc.next();
 
+            switch (s) {
+                case "push":
+                    push(sc.nextInt());
+                    break;
+
+                case "pop":
+                    stringBuilder.append(pop()).append("\n");
+                    break;
+
+                case "size":
+                    stringBuilder.append(size()).append('\n');
+                    break;
+
+                case "empty":
+                    stringBuilder.append(empty()).append('\n');
+                    break;
+
+                case "top":
+                    stringBuilder.append(top()).append('\n');
+                    break;
+            }
+        }
+        System.out.println(stringBuilder);
+
+    }
+
+    public static void push(int x){
+        stack[size] = x;
+        size++;
+    }
+
+    public static int pop() {
+        if(size == 0) return -1;
+        else {
+            int res = stack[size - 1];
+            stack[size - 1] = 0;
+            size--;
+            return res;
         }
     }
-    void push(int x){
 
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if(size == 0) return -1;
+        else return 0;
+    }
+
+    public static int top() {
+        if(size == 0) return -1;
+        else return stack[size - 1];
     }
 }
