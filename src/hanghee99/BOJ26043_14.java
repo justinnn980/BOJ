5d489d26699c22a50bdea418c370f021d356778a
justinnn980 <justinnn980@gmail.com>
2025-02-06 22:52:47 +0900

    항해99 14일차 BOJ26043 스택/큐 JAVA 진행 중

---
+++ src/hanghee99/BOJ26043_14.java
@@ -0,0 +1,14 @@
+package hanghee99;
+
+import java.util.Scanner;
+
+public class BOJ26043_14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+        }
+    }
+
+}
