072f076b351146a82df791fd4bdebd2ff1e43ef2
justinnn980 <justinnn980@gmail.com>
2025-02-08 23:29:33 +0900

    항해99 15일차 BOJ4949 스택 JAVA 진행 중

--- src/hanghee99/BOJ4949_15.java
+++ src/hanghee99/BOJ4949_15.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class BOJ4949_15 {
+
     public static int[] stack;
     public static int size = 0;
 
@@ -13,34 +14,32 @@ public class BOJ4949_15 {
 
         stack = new int[100];
 
-        while (true){
+        while (true) {
             String s = sc.nextLine();
-            
-            if (Objects.equals(s, ".")) break;
+
+            if (Objects.equals(s, ".")) {
+                break;
+            }
 
             char[] chars = s.toCharArray();
 
             for (int i = 0; i < chars.length; i++) {
                 char c = chars[i];
-                if (c == '(') {
+                if (c == '(' || c == '[') {
                     push(c);
-                } else if (c == ')') {
-                    pop(c);
-                } else if (c == '[') {
-                    push(c);
-                } else if (c == ']') {
-                    pop(c);
+                } else if (c == ')' || c == ']') {
+                    int re = pop(c);
+                    if (re == 7) {
+                        System.out.println("NO");
+                        break;
+                    }
                 } else if (c == '.') {
                     break;
                 }
             }
 
-
             if (stack[0] == 0) {
                 System.out.println("YES");
-                size = 0;
-            } else {
-                System.out.println("NO");
                 size = 0;
             }
         }
@@ -51,15 +50,17 @@ public class BOJ4949_15 {
         if (c == '(') {
             stack[size] = 1;
             size++;
+        } else if (c == '[') {
+            stack[size] = 2;
+            size++;
         }
-
     }
 
-    private static void pop(char c) {
-        if(size != 0){
-            stack[size - 1] = 0;
-            size--;
+    private static int pop(char c) {
+        if (size != 0) {
+            
         }
+        return 0;
     }
 
 }
