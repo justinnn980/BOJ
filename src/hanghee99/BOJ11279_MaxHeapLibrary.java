856c2eb1a4f276dcb8d041f99d0520389427d940
justinnn980 <justinnn980@gmail.com>
2025-02-12 23:17:04 +0900

    항해99 BOJ11279_MaxHeap, BOJ1927_MinHeap 최대힙,최소힙,라이브러리 버전까지 JAVA 완료

---
+++ src/hanghee99/BOJ11279_MaxHeapLibrary.java
@@ -0,0 +1,28 @@
+package hanghee99;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class BOJ11279_MaxHeapLibrary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        while (n-- > 0) {
+            int x = sc.nextInt();
+            if (x==0){
+                if(pq.isEmpty()){
+                    sb.append(x).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            }
+            else pq.offer(x);
+        }
+        System.out.println(sb);
+    }
+}
