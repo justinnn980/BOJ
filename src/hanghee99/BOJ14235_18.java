0cb43166f30d0becdf77848a93192407d5b9cbb1
justinnn980 <justinnn980@gmail.com>
2025-02-13 00:31:52 +0900

    항해99 18일차 BOJ14235 힙 JAVA 완료

--- src/hanghee99/BOJ14235_18.java
+++ src/hanghee99/BOJ14235_18.java
@@ -1,8 +1,33 @@
 package hanghee99;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class BOJ14235_18 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            if (a == 0) {
+                if(pq.isEmpty()){
+                    sb.append("-1").append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                for (int j = 0; j < a; j++) {
+                    pq.offer(sc.nextInt());
+                }
+            }
+        }
+        System.out.print(sb);
     }
 
 }
