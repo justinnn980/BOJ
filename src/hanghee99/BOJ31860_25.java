cc6b01277c289fb9e6b89ab67cb85e811f0ddc93
justinnn980 <justinnn980@gmail.com>
2025-02-21 18:41:22 +0900

    항해99 25일차 BOJ31860 열심히 힐하는 중 JAVA 진행 중

---
+++ src/hanghee99/BOJ31860_25.java
@@ -0,0 +1,35 @@
+package hanghee99;
+
+import java.util.Scanner;
+
+public class BOJ31860_25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        int good = 0;
+        int day = 0;
+        int dayp = 0;
+        int h = 0;
+        int[] arr = new int[n];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+            dayp += arr[i];
+        }
+        while (h < n) {
+
+            if (arr[h] / m > k) {
+                dayp++;
+            } else {
+                h++;
+            }
+
+        }
+
+        for (int i = 1; i <= day; i++) {
+            System.out.println(good);
+        }
+    }
+
+}
