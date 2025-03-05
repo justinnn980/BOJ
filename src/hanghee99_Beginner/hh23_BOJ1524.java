94bb5a38ece0b25c5eef780c30531986bff91be1
justinnn980 <justinnn980@gmail.com>
2025-03-05 18:22:18 +0900

    Save

--- src/hanghee99_Beginner/hh23_BOJ1524.java
+++ src/hanghee99_Beginner/hh23_BOJ1524.java
@@ -3,42 +3,34 @@ package hanghee99_Beginner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class hh23_BOJ1524 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { /// 버퍼리더 예외처리
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 0; i < T; i++) {
-            //테스트 케이스마다 한 줄을 비워서 입력받기 때문에 빈 줄을 받을 변수 L
-            String L = br.readLine();
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            //세준 세비의 제일 강한 병사의 힘을 저장할 변수
-            int Smax = 0;
-            int Bmax = 0;
+        int t = Integer.parseInt(st.nextToken());
 
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++) {
-                int S = Integer.parseInt(st.nextToken());
+        while (t-- > 0) {
 
-                if(Smax < S) Smax = S;
+            // 최대값 변수
+            int maxS = 0;  // 세준이 군대 최댓값
+            int maxB = 0;  // 세비 군대 최댓값
+
+            // 세준이 군대의 최댓값 찾기
+            
+            // 세비 군대의 최댓값 찾기
+
+            // 승자 판별
+            if (maxS > maxB) {
+                System.out.println("S");
+            } else if (maxS < maxB) {
+                System.out.println("B");
+            } else {
+                System.out.println("S");
             }
-
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++) {
-                int B = Integer.parseInt(st.nextToken());
-
-                if(Bmax < B) Bmax = B;
-            }
-
-            //힘이 더 쌘쪽이 이기고, 같으면 세준이가 이긴다.
-            if(Smax > Bmax) System.out.println("S");
-            else if(Bmax > Smax) System.out.println("B");
-            else System.out.println("S");
         }
     }
-
 }
