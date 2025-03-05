586990a2fc0d529383d04bc4a6bdf6a094e5bd7c
justinnn980 <justinnn980@gmail.com>
2025-03-06 00:16:35 +0900

    항해99 정렬 BOJ1524 세준세비 JAVA

--- src/hanghee99_Beginner/hh23_BOJ1524.java
+++ src/hanghee99_Beginner/hh23_BOJ1524.java
@@ -9,19 +9,32 @@ import java.util.StringTokenizer;
 public class hh23_BOJ1524 {
     public static void main(String[] args) throws IOException { /// 버퍼리더 예외처리
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int t = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(br.readLine());
 
         while (t-- > 0) {
+            String L = br.readLine();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
 
             // 최대값 변수
             int maxS = 0;  // 세준이 군대 최댓값
             int maxB = 0;  // 세비 군대 최댓값
 
             // 세준이 군대의 최댓값 찾기
-            
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                int S = Integer.parseInt(st.nextToken());
+
+                if(maxS < S) maxS = S;
+            }
             // 세비 군대의 최댓값 찾기
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++) {
+                int B = Integer.parseInt(st.nextToken());
+
+                if(maxB < B) maxB = B;
+            }
 
             // 승자 판별
             if (maxS > maxB) {
