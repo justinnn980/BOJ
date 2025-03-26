0b09b1337a3a91e5c75a9976f9076ce9bdd07af7
justinnn980 <justinnn980@gmail.com>
2025-03-26 17:58:34 +0900

    항해99 4주차 BOJ14235

--- src/hanghee99_Beginner/hh18_BOJ14235.java
+++ src/hanghee99_Beginner/hh18_BOJ14235.java
@@ -7,27 +7,52 @@ import java.util.Scanner;
 public class hh18_BOJ14235 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
+        int n = sc.nextInt();
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
-        int n = sc.nextInt();
-
         for (int i = 0; i < n; i++) {
             int a = sc.nextInt();
-            if (a == 0) {
-                if(pq.isEmpty()){
-                    sb.append("-1").append("\n");
-                } else {
-                    sb.append(pq.poll()).append("\n");
-                }
-            } else {
+
+            if(a != 0){
                 for (int j = 0; j < a; j++) {
                     pq.offer(sc.nextInt());
                 }
+            } else {
+                if(pq.isEmpty()){
+                    System.out.println(-1);
+                } else {
+                    System.out.println(pq.poll());
+                }
             }
+
         }
-        System.out.print(sb);
+
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        StringBuilder sb = new StringBuilder();
+//
+//        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+//
+//        int n = sc.nextInt();
+//
+//        for (int i = 0; i < n; i++) {
+//            int a = sc.nextInt();
+//            if (a == 0) {
+//                if(pq.isEmpty()){
+//                    sb.append("-1").append("\n");
+//                } else {
+//                    sb.append(pq.poll()).append("\n");
+//                }
+//            } else {
+//                for (int j = 0; j < a; j++) {
+//                    pq.offer(sc.nextInt());
+//                }
+//            }
+//        }
+//        System.out.print(sb);
     }
 
 }
