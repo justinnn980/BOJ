3ff0ba33a329bf65d8f7141b54a248426821d9a4
justinnn980 <justinnn980@gmail.com>
2025-03-31 08:45:08 +0900

    Update : bill

--- src/hanghee99_Beginner/hh20_BOJ28107.java
+++ src/hanghee99_Beginner/hh20_BOJ28107.java
@@ -13,7 +13,7 @@ public class hh20_BOJ28107 {
         Deque<Integer>[] orders = new ArrayDeque[200001];
         for (int i = 0; i < 200001; i++) orders[i] = new ArrayDeque<>();
 
-        int[] answer = new int[n];
+        int[] bill = new int[n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int k = Integer.parseInt(st.nextToken());
@@ -24,10 +24,14 @@ public class hh20_BOJ28107 {
         st = new StringTokenizer(br.readLine());
         while(m-->0) {
             int susi = Integer.parseInt(st.nextToken());
-            if (!orders[susi].isEmpty()) answer[orders[susi].pollFirst()] += 1; ///첫 번째 요소 제거 후 반환
+            if (!orders[susi].isEmpty()) {
+                bill[orders[susi].pollFirst()] += 1;
+            } ///첫 번째 요소 제거 후 반환
         }
 
-        for (int i = 0; i < n; i++) System.out.print(answer[i] + " ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(bill[i] + " ");
+        }
     }
 
 }
