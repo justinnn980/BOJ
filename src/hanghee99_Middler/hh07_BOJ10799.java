3db5f23ba53659df413329bed20c005c8efb2a9b
justinnn980 <justinnn980@gmail.com>
2025-04-08 17:14:35 +0900

    항해99 미들러 2주차 7일차 BOJ10799 수정 중

---
+++ src/hanghee99_Middler/hh07_BOJ10799.java
@@ -0,0 +1,16 @@
+package hanghee99_Middler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class hh07_BOJ10799 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String s = st.nextToken();
+
+    }
+}
