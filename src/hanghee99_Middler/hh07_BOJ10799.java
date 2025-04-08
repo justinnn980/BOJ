e4a48f13af297f3e626eb0ea1819c0f168b60967
justinnn980 <justinnn980@gmail.com>
2025-04-09 00:10:43 +0900

    항해99 미들러 2주차 7일자 BOJ10799 쇠막대기 자바

--- src/hanghee99_Middler/hh07_BOJ10799.java
+++ src/hanghee99_Middler/hh07_BOJ10799.java
@@ -16,13 +16,13 @@ public class hh07_BOJ10799 {
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (c == '(') {
-                stack.push(c);
+                stack.push(c); /// 열린 괄호면 스택에 넣음
             } else {
-                stack.pop();
+                stack.pop(); /// 닫힌 괄호면 스택에서 하나 꺼냄
                 if (input.charAt(i-1) == '(') {
                     result += stack.size(); /// 이전 문자가 '(' 이면 레이저 -> 현재 스택 크기만큼 조각 추가
                 } else {
-                    result += 1; ///쇠막대기의 끝이면 조각 하나 추가
+                    result += 1; /// 쇠막대기의 끝이면 조각 하나 추가
                 }
             }
         }
