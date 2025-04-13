25bce5e6811c7622d2d4868fa0f4827ba2fb2df4
justinnn980 <justinnn980@gmail.com>
2025-04-14 00:03:54 +0900

    항해99 미들러 DFS/BFS

--- src/hanghee99_Middler/hh00_BOJ1260.java
+++ src/hanghee99_Middler/hh00_BOJ1260.java
@@ -3,82 +3,71 @@ package hanghee99_Middler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class hh00_BOJ1260 {
-    static StringBuilder sb = new StringBuilder();
-    static boolean[] check;
-    static int[][] arr;
-
-    static int node, line, start;
-
-    static Queue<Integer> q = new LinkedList<>();
+    static boolean[] visited;
+    static int[][] graph;
+    static int n;
 
     public static void main(String[] args) throws IOException {
         ///입력 N M V 정점의 개수, 간선의 개수, 정점의 번호
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        node = Integer.parseInt(st.nextToken());
-        line = Integer.parseInt(st.nextToken());
-        start= Integer.parseInt(st.nextToken());
 
-        arr = new int[node+1][node+1];
-        check = new boolean[node+1];
+        n = Integer.parseInt(st.nextToken()); // 정점 개수
+        int m = Integer.parseInt(st.nextToken()); // 간선 개수
+        int v = Integer.parseInt(st.nextToken()); // 시작 정점
 
-        for(int i = 0 ; i < line ; i ++) {
-            StringTokenizer str = new StringTokenizer(br.readLine());
+        graph = new int[n + 1][n + 1];
 
-            int a = Integer.parseInt(str.nextToken());
-            int b = Integer.parseInt(str.nextToken());
+        // 간선 정보 입력
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            arr[a][b] = arr[b][a] =  1;
+            graph[a][b] = 1;
+            graph[b][a] = 1;
         }
 
-        ///출력 DFS로 탐색할때 노드를 순차적으로 첫줄에 출력
-        /// BFS 로 탐색할때 노드를 순차적으로 둘째줄에 출력
-        //sb.append("\n");
-        dfs(start);
-        sb.append("\n");
-        check = new boolean[node+1];
+        visited = new boolean[n + 1];
+        dfs(v);
+        System.out.println();
 
-        bfs(start);
-
-        System.out.println(sb);
-
+        visited = new boolean[n + 1];
+        bfs(v);
+        System.out.println();
     }
-    ///로직 주어진 간선리스트를 통해서 DFS와 BFS 구현
 
-    public static void dfs(int start) {
+    // DFS 구현 (재귀)
+    public static void dfs(int v) {
+        visited[v] = true;
+        System.out.print(v + " ");
 
-        check[start] = true;
-        sb.append(start + " ");
-
-        for(int i = 0 ; i <= node ; i++) {
-            if(arr[start][i] == 1 && !check[i])
+        for (int i = 1; i <= n; i++) {
+            if (graph[v][i] == 1 && !visited[i]) {
                 dfs(i);
-        }
-
-    }
-
-    public static void bfs(int start) {
-        q.add(start);
-        check[start] = true;
-
-        while(!q.isEmpty()) {
-
-            start = q.poll();
-            sb.append(start + " ");
-
-            for(int i = 1 ; i <= node ; i++) {
-                if(arr[start][i] == 1 && !check[i]) {
-                    q.add(i);
-                    check[i] = true;
-                }
             }
         }
     }
 
+    // BFS 구현 (큐 사용)
+    public static void bfs(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+        visited[v] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            System.out.print(cur + " ");
+
+            for (int i = 1; i <= n; i++) {
+                if (graph[cur][i] == 1 && !visited[i]) {
+                    queue.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
 }
