358a7fdcd9dd3aaf18a22ceae355fe50551b0899
justinnn980 <justinnn980@gmail.com>
2025-04-15 20:57:31 +0900

    항해99 미들러 3주차 12일차 BOJ2156 포도주 시식 JAVA 수정

--- src/hanghee99_Middler/hh12_BOJ2156.java
+++ src/hanghee99_Middler/hh12_BOJ2156.java
@@ -18,22 +18,28 @@ public class hh12_BOJ2156 {
         }
 
         /// 로직
-        int[] dp = new int[n];
-        dp[0] = arr[0];
+        int[] dp = new int[n+1];
+        dp[1] = arr[0];
 
-        for (int i = 1; i < n; i++) {
-            if(i==1){
-                dp[1] = arr[0] + arr[1];
-                continue;
-            }
-            if(i==2){
-                dp[2] = Math.max(dp[1], Math.max(arr[0]+arr[2], arr[1]+arr[2]));
-                continue;
-            }
-            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i-1] + arr[i]));
+        if(n <= 1){
+            System.out.println(dp[n]);
+            return;
+        }
+
+        dp[2] = dp[1] + arr[1];
+
+        if(n <= 2){
+            System.out.println(dp[n]);
+            return;
+        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-3] + arr[i-2] + arr[i-1];
+            dp[i] = Math.max(dp[i], dp[i-2] + arr[i-1]);
+            dp[i] = Math.max(dp[i], dp[i-1]);
         }
 
         /// 출력
-        System.out.println(dp[n-1]);
+        System.out.println(dp[n]);
     }
 }
