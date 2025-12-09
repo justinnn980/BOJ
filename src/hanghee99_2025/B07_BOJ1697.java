9cade82924e9e3e2180af448bbcb60176941b0c3
justinnn980 <justinnn980@gmail.com>
2025-12-09 23:10:38 +0900

    Update : BOJ1697 다시해야함

---
+++ src/hanghee99_2025/B07_BOJ1697.java
@@ -0,0 +1,53 @@
+package hanghee99_2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class B07_BOJ1697 {
+    static final int Max = 100000;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int result = bfs(n,k);
+        System.out.println(result);
+    }
+    private static int bfs(int start, int end) {
+        if(start == end) return 0;
+        Queue<Integer> queue = new LinkedList<>();
+        int[] time = new int[Max+1];
+        boolean[] visited = new boolean[Max+1];
+
+        queue.add(start);
+        visited[start] = true;
+        time[start] = 0;
+
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            int[] nextPositions = {now - 1, now + 1
+                , now * 2};
+
+            for(int next : nextPositions){
+                if(next >= 0 && next <= Max && !visited[next]){
+                    visited[next] = true;
+                    time[next] = time[now] + 1;
+                    queue.add(next);
+                    if(next == end) {
+                        return time[next];
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+}
+
+
