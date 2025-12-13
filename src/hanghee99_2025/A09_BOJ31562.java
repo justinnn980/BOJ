4659e2863ddde1912c8a6846dff27ec49edf932b
justinnn980 <justinnn980@gmail.com>
2025-12-13 14:04:08 +0900

    Update : BOJ31562 Map 사용법 : 키 값 put 값넣기 get 값꺼내기 key 존재여부확인 등등

--- src/hanghee99_2025/A09_BOJ31562.java
+++ src/hanghee99_2025/A09_BOJ31562.java
@@ -18,13 +18,23 @@ public class A09_BOJ31562 {
 
         for (int i = 0; i < N; i++) {
             String[] s = br.readLine().split(" ",3);
-            String value = s[2].substring(0, 5);
+//            String value = s[2].substring(0, 5);
 
+//            map.put(value, map.containsKey(value) ? "?\n" : s[1] + "\n");
 
-            map.put(value, map.containsKey(value) ? "?\n" : s[1] + "\n");
+            String title = s[0];
+            String singer = s[1];
+            String melody = s[2].substring(0, 5);
+
+            if (map.containsKey(melody)) {
+                map.put(melody, "?");
+            } else {
+                map.put(melody, singer);
+            }
         }
-        while (M-- > 0) {
-            System.out.print(map.getOrDefault(br.readLine(), "!\n"));
+        for (int i = 0; i < M; i++) {
+            String query = br.readLine();
+            System.out.println(map.getOrDefault(query, "!"));
         }
     }
 
