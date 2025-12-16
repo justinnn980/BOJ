a408ac869c1eb9cf933ccbab5e48c5c7174d9f43
justinnn980 <justinnn980@gmail.com>
2025-12-17 00:01:39 +0900

    Update : BOJ10828 Tip : runtime error를 위해 sb 사용

---
+++ src/hanghee99_2025/A12_BOJ10828.java
@@ -0,0 +1,65 @@
+package hanghee99_2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class A12_BOJ10828 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+
+            switch (s) {
+                case "push":
+                    stack.push(Integer.valueOf(st.nextToken()));
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        sb.append("-1\n");
+                        break;
+                    } else {
+                        sb.append(stack.pop());
+                        sb.append("\n");
+                        break;
+                    }
+                case "size":
+                    sb.append(stack.size());
+                    sb.append("\n");
+                    break;
+                case "empty":
+                    if (stack.isEmpty()) {
+                        sb.append("1");
+                        sb.append("\n");
+                        break;
+                    } else {
+                        sb.append("0");
+                        sb.append("\n");
+                        break;
+                    }
+                case "top":
+                    if (stack.isEmpty()) {
+                        sb.append("-1");
+                        sb.append("\n");
+                        break;
+                    } else {
+                        sb.append(stack.peek());
+                        sb.append("\n");
+                        break;
+                    }
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
