e6e97f5fd8745a85f0ce29d07f854f36c3267e74
justinnn980 <justinnn980@gmail.com>
2025-12-18 23:43:46 +0900

    Update : BOJ10845 진행중

---
+++ src/hanghee99_2025/B06_BOJ1260_2.java
@@ -0,0 +1,10 @@
+package hanghee99_2025;
+
+import java.util.*;
+
+public class B06_BOJ1260_2 {
+    public static void main(String[] args) {
+        B06_BOJ1260_2 B06_BOJ1260_2 = new B06_BOJ1260_2();
+        B06_BOJ1260_2.solve();
+    }
+}
