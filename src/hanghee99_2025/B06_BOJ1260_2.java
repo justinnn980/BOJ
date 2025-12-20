7043794513f3239c72f81a534cb7526d97ad2071
justinnn980 <justinnn980@gmail.com>
2025-12-20 20:40:15 +0900

    Update : BOJ1260 Tip : 진행 중

--- src/hanghee99_2025/B06_BOJ1260_2.java
+++ src/hanghee99_2025/B06_BOJ1260_2.java
@@ -6,34 +6,39 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class B06_BOJ1260_2 {
+    static StringBuilder sb = new StringBuilder();
     static boolean[] visited;
     static ArrayList<Integer>[] list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int start = Integer.parseInt(st.nextToken());
         list = new ArrayList[N + 1];
+
+        // 초기화 하기
         for (int i = 1; i <= N; i++) {
-            list[i] = new ArrayList<>(); // 인접리스트 완성
+            list[i] = new ArrayList<>(); // 초기화
         }
+        // 그래프 데이터 저장하기
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int S = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
             list[S].add(E);
             list[E].add(S);
-            // 그래프 완성
         }
-        
-//        for (int i = 1; i <= N; i++) {
-//            Collections.sort(list[i]);
-//        }
+
+        // 각 노드를 돌면서 오름차순 정렬
+        for (int i = 1; i <= N; i++) {
+            list[i].sort(null);
+        }
 
         visited = new boolean[N + 1];
         DFS(start);
-        System.out.println();
+        System.out.println(sb.toString());
         visited = new boolean[N + 1];
         BFS(start);
         System.out.println();
@@ -44,7 +49,7 @@ public class B06_BOJ1260_2 {
         queue.add(start);
         visited[start] = true;
         while (!queue.isEmpty()) {
-            int cur = queue.poll();
+            int cur = queue.poll(); // cur = 노드에 연결된 엣지 정보를 알고싶다
             System.out.print(cur+" ");
             for (int i = 0; i < list[cur].size(); i++) {
                 if (!visited[list[cur].get(i)]) {
@@ -56,11 +61,12 @@ public class B06_BOJ1260_2 {
     }
 
     private static void DFS(int start) {
-        System.out.print(start+" ");
-        visited[start] = true;
-        for (int i : list[start]) {
-            if (!visited[i]) {
-                DFS(i);
+        sb.append(start).append(" "); // 방문값 넣기
+        visited[start] = true; // 방문하면 true
+        for (int i = 0; i < list[start].size(); i++) {
+            int idx = list[start].get(i); //리스트 1부터 돌면서
+            if (!visited[idx]) {//방문하지 않은 노드면
+                DFS(idx); // 반복
             }
         }
     }
