06abd61468ab7e07d6f1e323f184129947ef03ee
justinnn980 <justinnn980@gmail.com>
2025-12-29 01:52:47 +0900

    Update : BOJ14235 Tip : PriorityQueue<>(Collections.reverseOrder());

--- src/hanghee99_2025/A18_BOJ14235.java
+++ src/hanghee99_2025/A18_BOJ14235.java
@@ -1,5 +1,35 @@
 package hanghee99_2025;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 public class A18_BOJ14235 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            if (a != 0){
+                while (a-->0) {
+                    pq.offer(Integer.parseInt(st.nextToken()));
+                }
+            } else {
+                if (pq.isEmpty()) {
+                    System.out.println("-1");
+                } else {
+                    System.out.println(pq.poll());
+                }
+            }
+        }
+    }
 
 }
