8e1fa4e9cb62b88de56061367598eadb68140230
justinnn980 <justinnn980@gmail.com>
2026-01-03 23:35:02 +0900

    Update : BOJ15686 Tip : 진행중

---
+++ src/hanghee99_2025/B15_BOJ15686.java
@@ -0,0 +1,16 @@
+package hanghee99_2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B15_BOJ15686 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+    }
+
+}
