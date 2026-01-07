38228bc9397dac9157165e5b49bc7d23d3e1cfe2
justinnn980 <justinnn980@gmail.com>
2026-01-08 01:57:52 +0900

    feat(BOJ27961): 입력값에 따라 2의 거듭제곱 계산 추가
    - 입력값 N이 0일 경우 0을 출력하도록 수정
    - N보다 작은 2의 거듭제곱을 찾고, 생성 및 복제 횟수를 계산하여 출력
    - untitled.iml 파일 이름을 Kotlin2026.iml로 변경

--- src/hanghee99_2025/B16_BOJ27961.java
+++ src/hanghee99_2025/B16_BOJ27961.java
@@ -3,13 +3,25 @@ package hanghee99_2025;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class B16_BOJ27961 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-    }
+        long N = Long.parseLong(br.readLine().trim());
 
+        if (N == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        long pow = 1; // 2^t
+        int t = 0;
+        while (pow < N) {
+            pow <<= 1;
+            t++;
+        }
+
+        // 생성 1번 + 복제 t번
+        System.out.println(t + 1);
+    }
 }
