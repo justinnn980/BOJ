dba08284e36215b8ca1351d92a55fbd521566399
justinnn980 <justinnn980@gmail.com>
2026-01-18 21:21:07 +0900

    save

---
+++ src/hanghee99_2025/B19_BOJ1946.java
@@ -0,0 +1,13 @@
+package hanghee99_2025;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B19_BOJ1946 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+
+}
