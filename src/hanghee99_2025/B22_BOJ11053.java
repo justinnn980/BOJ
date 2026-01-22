e3b990f184d2904e25684a8602c4e81758ef0dd2
justinnn980 <justinnn980@gmail.com>
2026-01-22 12:48:38 +0900

    Update : BOJ11053 최대 값으로 비교해서 가장 크면 숫자 증가, 가장 크게 증가한 값 출력

---
+++ src/hanghee99_2025/B22_BOJ11053.java
@@ -0,0 +1,43 @@
+package hanghee99_2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B22_BOJ11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+
+        // 모든 위치에서 최소 길이는 1
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+    }
+
+}
