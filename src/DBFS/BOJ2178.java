f7ceef2e0685c3ae66e554484984c6f6dc2c3c42
justinnn980 <justinnn980@gmail.com>
2026-01-30 02:02:04 +0900

    Update : 정리 중

---
+++ src/DBFS/BOJ2178.java
@@ -0,0 +1,20 @@
+package DBFS;
+
+public class BOJ2178 {
+    static int[][] map;
+    static int N, M;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int result = 0;
+    public static void main(String[] args) {
+
+    }
+    static void dfs(int x, int y){
+
+    }
+    static void bfs(int x, int y){
+
+    }
+
+
+}
