d2d3e2d85718fbfe7de218b884700444b2c3f9fb
jc <justinnn980@gmail.com>
2026-03-03 15:33:29 +0900

    Update : SAVE

---
+++ src/Blog/Blog14_BOJ9663.java
@@ -0,0 +1,25 @@
+package Blog;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Blog14_BOJ9663 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(solution(N));
+    }
+
+    // 👉 프로그래머스 스타일
+    public static int solution(int n) {
+
+        int answer = 0;
+
+        // 여기서 n을 사용해 로직 작성
+
+        return answer;
+    }
+}
