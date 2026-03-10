af4e46d7ed8b30a467589edee0f2958212761992
jc <justinnn980@gmail.com>
2026-03-10 22:32:54 +0900

    Update : Ai day3-2

---
+++ src/AI/AI11_BOJ2667.java
@@ -0,0 +1,75 @@
+package AI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class AI11_BOJ2667 {
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int N;
+    static int[][] map;
+    static PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+    static boolean[][] visited;
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    int size = solu(i, j);
+                    pq.add(size);
+                }
+            }
+        }
+        System.out.println(pq.size());
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll());
+        }
+    }
+
+
+    private static int solu(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x,y});
+        visited[x][y] = true;
+
+        count = 1;
+
+        while (!q.isEmpty()){
+            int[] cur = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if(nx<0 || ny<0 || nx>=N || ny>=N) continue;
+                if(visited[nx][ny]) continue;
+                if(map[nx][ny]==0) continue;
+
+                visited[nx][ny] = true;
+                count++;
+                q.offer(new int[]{nx, ny});
+            }
+
+        }
+        return count;
+    }
+}
