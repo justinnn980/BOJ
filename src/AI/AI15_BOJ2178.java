c01c41c9f4a5ca3679ecd4f16f156c57be0ee6b5
jc <justinnn980@gmail.com>
2026-03-11 14:53:30 +0900

    Update : Ai day4-2

---
+++ src/AI/AI15_BOJ2178.java
@@ -0,0 +1,61 @@
+package AI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class AI15_BOJ2178 {
+    static int N,M;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int[][] map;
+    static boolean[][] visited;
+    static int[][] dist;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(re(N,M,map));
+    }
+
+    private static int re(int n, int m, int[][] map) {
+        visited[0][0] = true;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0,0});
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx<0 || ny<0 || nx>=N || ny >= M) continue;
+                if (visited[nx][ny]) continue;
+                if (map[nx][ny] == 0) continue;
+
+                // map[n][m] == map[n-1][m-1] + 1
+                map[nx][ny] = map[cur[0]][cur[1]] + 1;
+                visited[nx][ny] = true;
+                q.offer(new int[]{nx, ny});
+            }
+        }
+        return map[n-1][m-1];
+    }
+}
