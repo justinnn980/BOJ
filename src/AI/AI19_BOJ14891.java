a0065e4d75836098af9e09126ae7fe37c6fb0e6d
jc <justinnn980@gmail.com>
2026-03-13 00:06:26 +0900

    Update : Ai day6-1 완료

--- src/AI/AI19_BOJ14891.java
+++ src/AI/AI19_BOJ14891.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class A19_BOJ14891 {
+public class AI19_BOJ14891 {
     static int K;
     static int[][] gear;
     public static void main(String[] args) throws IOException {
---
+++ src/AI/AI20_BOJ7576.java
@@ -0,0 +1,76 @@
+package AI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class AI20_BOJ7576 {
+    static int M, N;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] map;
+    static int day, max;
+    static Queue<int[]> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+                if (map[i][j] == 1) {
+                    q.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        System.out.println(sol());
+    }
+
+    private static int sol() {
+        bfs();
+
+        day = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) {
+                    return -1;
+                }
+                max = Math.max(max, map[i][j]);
+            }
+        }
+        return max - 1;
+    }
+
+    private static void bfs() {
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (map[nx][ny] != 0) continue;
+
+                map[nx][ny] = map[x][y] + 1;
+                q.offer(new int[]{nx, ny});
+            }
+
+        }
+    }
+
+}
