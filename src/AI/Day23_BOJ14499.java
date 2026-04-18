1bed68f63389664043ead6663b34f5280bb5ddd0
jc <justinnn980@gmail.com>
2026-04-18 22:04:38 +0900

    Update : Commit

---
+++ src/AI/Day23_BOJ14499.java
@@ -0,0 +1,13 @@
+package AI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Day23_BOJ14499 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
